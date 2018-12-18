Fix calculation of FN in genvideo_cost/prob.py

FCN_tensorflow/genVideo_costmap.py
@@ -5,9 +5,9 @@ import matplotlib.pyplot as plot
 import matplotlib.image as mpimg
 import pdb
 
-PATH = '/home/gaobiao/Documents/RoadSegmentation_IV2019/results/1121_weaklabel_costmap/vis/'
-NAME = '1121_weaklabel_costmap'
-DATA_PATH = '/home/gaobiao/Documents/RoadSegmentation_IV2019/data/data_guilin_1121/'
+PATH = '/home/gaobiao/Documents/RoadSegmentation_IV2019/results/1216_2label_weak/vis/'
+NAME = '1216_2label_weak'
+DATA_PATH = '/home/gaobiao/Documents/RoadSegmentation_IV2019/data/data_easy/'
 
 IS_WEAK_LABEL = True
 IMAGE_WIDTH = 300
@@ -19,7 +19,7 @@ def ColorMap(data):
         data = [0, 0, 0]
     elif data[0] == 1:      # passable
         data = [0, 255, 0]
-    elif data[0] == 2:      # impassable
+    elif data[0] == 2:      # iprecisionssable
         data = [0, 0, 255]
     elif data[0] == 3:      # uncertain
         data = [255, 0, 0]
@@ -62,10 +62,11 @@ def OutputResult(table, baseline):
             fout.write('%d\t' % table[i,j])
         fout.write('\n')
     fout.write('---------------------------\n')
-    fout.write('label\ttp\tfp\tfn\tIoU\tmPA\n')
+    fout.write('label\ttp\tfp\tfn\tIoU\trecall\tprecision\n')
     for i in range(1,NUM_OF_CLASSESS):
         tp = fp = fn = cn = 0
         tp = table[i,i].astype(int)
+        fn += table[i,0]
         for j in range(1, NUM_OF_CLASSESS):
             if i != j:
                 fp += table[j,i].astype(int)
@@ -74,11 +75,17 @@ def OutputResult(table, baseline):
             IoU = float(tp) / float(tp + fp + fn)
         else:
             IoU = 0
-        if (tp + fp != 0):
-            mPA = float(tp)/float(tp+fp)
+
+        if (tp + fn != 0):
+            recall = float(tp) / float(tp + fn)
         else:
-            mPA = 0
-        fout.write('%d\t%d\t%d\t%d\t%.6f\t%.6f\n' % (int(i), int(tp), int(fp), int(fn), float(IoU), float(mPA)))
+            recall = 0
+
+        if (tp + fp != 0):
+            precision = float(tp)/float(tp+fp)
+        else:
+            precision = 0
+        fout.write('%d\t%d\t%d\t%d\t%.6f\t%.6f\t%.6f\n' % (int(i), int(tp), int(fp), int(fn), float(IoU), float(recall), float(precision)))
 
     fout.write('-------------Baseline--------------\n')
     for i in range(NUM_OF_CLASSESS):
@@ -86,10 +93,11 @@ def OutputResult(table, baseline):
             fout.write('%d\t' % baseline[i,j])
         fout.write('\n')
     fout.write('---------------------------\n')
-    fout.write('label\ttp\tfp\tfn\tIoU\tmPA\n')
+    fout.write('label\ttp\tfp\tfn\tIoU\trecall\tprecision\n')
     for i in range(1,NUM_OF_CLASSESS):
         tp = fp = fn = cn = 0
         tp = baseline[i,i].astype(int)
+        fn += table[i,0]
         for j in range(1, NUM_OF_CLASSESS):
             if i != j:
                 fp += baseline[j,i].astype(int)
@@ -98,11 +106,17 @@ def OutputResult(table, baseline):
             IoU = float(tp) / float(tp + fp + fn)
         else:
             IoU = 0
-        if (tp + fp != 0):
-            mPA = float(tp)/float(tp+fp)
+
+        if (tp + fn != 0):
+            recall = float(tp) / float(tp + fn)
         else:
-            mPA = 0
-        fout.write('%d\t%d\t%d\t%d\t%.6f\t%.6f\n' % (int(i), int(tp), int(fp), int(fn), float(IoU), float(mPA)))
+            recall = 0
+
+        if (tp + fp != 0):
+            precision = float(tp)/float(tp+fp)
+        else:
+            precision = 0
+        fout.write('%d\t%d\t%d\t%d\t%.6f\t%.6f\t%.6f\n' % (int(i), int(tp), int(fp), int(fn), float(IoU), float(recall), float(precision)))
     fout.close()
 
 
@@ -134,7 +148,7 @@ gtList.sort()
 preList.sort()
 costmapList.sort()
 
-videoWriter = cv2.VideoWriter(NAME+'.avi', cv2.cv.CV_FOURCC('M', 'J', 'P', 'G'), 10, (IMAGE_WIDTH * 3, IMAGE_HEIGHT * 2), True)
+videoWriter = cv2.VideoWriter(NAME+'.avi', cv2.cv.CV_FOURCC('M', 'J', 'P', 'G'), 10, (IMAGE_WIDTH * 4, IMAGE_HEIGHT * 1), True)
 #videoWriter = cv2.VideoWriter('test.avi', cv2.VideoWriter_fourcc(*'XVID'), 5, (IMAGE_WIDTH, IMAGE_HEIGHT * 2), True)
 
 for i in range(len(imgList)):
@@ -146,24 +160,27 @@ for i in range(len(imgList)):
     videoImg = cv2.imread(DATA_PATH + "video/" + imgList[i].split('.')[0].split('_')[1] + ".png", cv2.IMREAD_COLOR)
     # gt = cv2.flip(gt, 0)
     pre = cv2.imread(PATH + preList[i], cv2.IMREAD_COLOR)
+    gt = preProcess(img, gt)
     pre = preProcess(img, pre)
     costMap = cv2.imread(PATH + costmapList[i], cv2.IMREAD_COLOR)
 
     gtClone = gt.copy()
     gtClone1 = gt.copy()
+    IS_WEAK_LABEL = False
     table = LabelColor(img, gt, pre)
     cntTable += table
     table = LabelColor(img, gtClone, bgt)
     cntTableBaseline += table
+    IS_WEAK_LABEL = True
     none = LabelColor(img, wgt, gtClone1)
 
-    # colorizedImg = cv2.applyColorMap(costMap, cv2.COLORMAP_RAINBOW)
+    colorizedImg = cv2.applyColorMap(costMap, cv2.COLORMAP_HOT)
     videoImg = cv2.resize(videoImg, (IMAGE_WIDTH, IMAGE_HEIGHT), interpolation=cv2.INTER_NEAREST)
-    mergeImg0 = np.concatenate((img, wgt, pre), axis=1)
-    mergeImg1 = np.concatenate((videoImg, bgt, gt), axis=1)
-    mergeImg = np.concatenate((mergeImg0, mergeImg1), axis=0)
+    mergeImg0 = np.concatenate((img, wgt, pre,gt), axis=1)
+    # mergeImg1 = np.concatenate((videoImg, colorizedImg, gt), axis=1)
+    # mergeImg = np.concatenate((mergeImg0, mergeImg1), axis=0)
     # mergeImg = cv2.flip(mergeImg, 0)
-    videoWriter.write(mergeImg)
+    videoWriter.write(mergeImg0)
     print('Frame: %d / %d' % (i, len(imgList)))
 
 OutputResult(cntTable, cntTableBaseline)